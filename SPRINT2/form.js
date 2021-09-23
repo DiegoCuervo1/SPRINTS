ec34bb7f77c38b785f30f529c64ce9f76f8f5a7d
Cesar Macea <cdmacea@gmail.com>
2021-09-23 18:34:57 -0500

    form javascript final

--- SPRINT2/form.js
+++ SPRINT2/form.js
@@ -1,53 +1,60 @@
-function checkNombre(valor) {
-    var resc = /^[a-zA-Z]{4,30}$/;
-    if (resc.test(valor)) {
-        return true;
-    } else {
+const nombre=document.getElementById("nombre");
+function checkNombre(nombre){
+      if (/^([a-zA-Z]{4,30})+$/.test(nombre)) {
+          return true;   
+      }else{
+        
         return false;
+      }
+      
+  }
+ 
+  module.exports.checkNombre=checkNombre;
+
+  var apellido=document.getElementById("apellido");
+ function checkApellido(apellido){
+      if (/^([a-zA-Z]{4,30})+$/.test(apellido)) {
+          return true;  
+      }else{
+           return false;
+          
+      }
+  }
+
+
+  module.exports.checkApellido=checkApellido;
+  
+ var telefono=document.getElementById("telefono");
+ 
+ function checkTelefono(telefono){
+      if (/^([0-9]{7})+$/.test(telefono)) {  
+        return true;   
+      }else{ 
+         return false;
+      }  
+  }
+  module.exports.checkTelefono = checkTelefono;
+  
+  var correo = document.getElementById("correo");
+function checkCorreo(correo){
+    if (/^\D[a-zA-Z0-9_-]+\@+[a-zA-Z]+(\.+[a-zA-Z]+)?(\.+([a-zA-Z])+)?(\.+([a-zA-Z])+)?\.([a-zA-Z]{2,3})+$/.test(correo)) {
+      return true;    
+       
+    }else{
+      return false;
     }
+  return n;
+    
 }
-exports = checkNombre;
+module.exports.checkCorreo = checkCorreo;
 
-
-function checkApellido(valor) {
-    var resc = /^[a-zA-Z]{4,30}$/;
-    if (resc.test(valor)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-exports = checkApellido;
-
-
-function checkTelefono(valor) {
-    var resc = /^[0-9]{7}$/;
-    if (resc.test(valor)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-exports = checkTelefono;
-
-
-function checkCorreo(valor) {
-    var resc = /^\D[a-zA-Z0-9_-]+\@+[a-zA-Z]+(\.+[a-zA-Z]+)?(\.+([a-zA-Z])+)?(\.+([a-zA-Z])+)?\.([a-zA-Z]{2,3})$/;
-    if (resc.test(valor)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-exports = checkCorreo;
-
-
-function checkContrasena(valor) {
-    var resc = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z\d$@$!%*?&]{8,}[^'\s]$/;
-    if (resc.test(valor)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-exports = checkContrasena;
+var contrasena=document.getElementById("contrasena");
+ function checkContrasena(contrasena){
+      if ( /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z\d$@$!%*?&]{8,}[^'\s]+$/.test(contrasena)) {
+        return true;   
+      }else{
+          return false;
+      }
+      
+  }
+  module.exports.checkContrasena = checkContrasena;
